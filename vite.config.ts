7d8e944fb1efef3ccbc80ca476ea9872a75617f3
Edward Arechwa <e.arechwa@digitalcolliers.com>
2022-04-18 19:26:51 +0200

    add base aliases

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,11 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@common': path.resolve(__dirname, './src/common'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@context': path.resolve(__dirname, './src/context'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@store': path.resolve(__dirname, './src/store'),
     },
   },
 });
