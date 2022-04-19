53ed75ad4990dc2e3f985e592f0dc22812766b99
Edward Arechwa <e.arechwa@digitalcolliers.com>
2022-04-19 12:51:46 +0200

    add story for button

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 const path = require('path');
+const svgrPlugin = require('vite-plugin-svgr');
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
