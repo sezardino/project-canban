ad882decfb1c6255b95b2dceb65b2274ceecf75e
Edward Arechwa <e.arechwa@digitalcolliers.com>
2022-04-21 17:27:01 +0200

    add column

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,2 +1,3 @@
 export * from './Toast/Toast';
 export * from './Card/Card';
+export * from './Column/Column';
--- src/components/pages/Board/Board.tsx
+++ src/components/pages/Board/Board.tsx
@@ -1,14 +1,13 @@
-import { Card } from '@/components/molecules';
+import { Card, Column } from '@/components/molecules';
 
 import { tasks } from './mock';
 
 export const Board: React.FC = () => {
   return (
     <div>
-      <h1>Board1</h1>
-      {tasks.map((task) => (
-        <Card key={task.id} {...task} />
-      ))}
+      <Column label="TODO" items={tasks}>
+        {(task) => <Card {...task} />}
+      </Column>
     </div>
   );
 };
