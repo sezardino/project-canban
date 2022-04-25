6b919da8818312c4539fa0de6b02fc7c5b434603
Edward Arechwa <e.arechwa@digitalcolliers.com>
2022-04-25 14:14:26 +0200

    add client for data

---
+++ src/client/types.ts
@@ -0,0 +1,6 @@
+export interface Client<T extends { id: string }> {
+  getAll: () => Promise<T[]>;
+  getById: (id: string) => Promise<T>;
+  update: (item: T) => Promise<T>;
+  add: (item: T) => Promise<T[]>;
+}
