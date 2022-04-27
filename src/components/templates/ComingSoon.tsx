c2b240bf7cc299bef9aea85c287290b80c95ccb7
Edward Arechwa <e.arechwa@digitalcolliers.com>
2022-04-27 12:28:53 +0200

    add comming soon template

---
+++ src/components/templates/ComingSoon.tsx
@@ -0,0 +1,19 @@
+import { Button, Heading } from '@/components';
+import { ReactComponent as Logo } from '@/assets/logo-full.svg';
+
+export const ComingSoon = () => {
+  return (
+    <div className="container mx-auto h-full text-center py-40">
+      <Logo width="250" height="250" className="mx-auto" />
+      <Heading type="h1">New Things Coming Soon</Heading>
+      <div className="mt-10 flex gap-2 justify-center">
+        <Button to={'/release'} isOutlined color="dark" size="lg">
+          Stay tuned for updates
+        </Button>
+        <Button to={'/demo'} isOutlined color="dark" size="lg">
+          Try the demo for now
+        </Button>
+      </div>
+    </div>
+  );
+};
