fd9e8c1c2d9e78a8054eacc97896c96b01305c18
Edward Arechwa <e.arechwa@digitalcolliers.com>
2022-04-27 14:59:05 +0200

    upd default layout

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -1,11 +1,39 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import { ToastsWrapper } from '../organisms/ToastsWrapper/ToastsWrapper';
+
+import { ReactComponent as Logo } from '@/assets/logo.svg';
+import { Button } from '@/components';
+import { indexPages } from '@/router/pages';
 
 export const DefaultLayout: React.FC = () => {
   return (
-    <main className="py-20 container mx-auto grid content-start gap-8">
-      <Outlet />
-      <ToastsWrapper className="fixed top-10 left-full w-full max-w-xs translate-x-[-110%]" />
-    </main>
+    <>
+      <header className="py-5 shadow-2xl">
+        <div className="container mx-auto flex items-center justify-between">
+          <Link to="/">
+            <Logo />
+          </Link>
+          <nav>
+            <ul className="flex items-center">
+              {indexPages.map(({ path, label }) => (
+                <>
+                  {!label ? null : (
+                    <li key={path}>
+                      <Button to={path} isLink hasPadding color="dark">
+                        {label}
+                      </Button>
+                    </li>
+                  )}
+                </>
+              ))}
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <main>
+        <Outlet />
+        <ToastsWrapper className="fixed top-10 left-full w-full max-w-xs translate-x-[-110%]" />
+      </main>
+    </>
   );
 };
